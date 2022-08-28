f849aeb6dab7fe3ee56f3ffd5e43a5c66bbbb570
tjphillips08 <tjphillips08@icloud.com>
2022-08-28 15:09:36 -0500

    express boilerplate

---
+++ controllers/game_controller.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const router = express.Router();
+
+
+//Middleware: will have express json and url encoded eventually
+
+
+//Model import here
+//const db = require('..models')
+
+
+//Routes will be here
+
+
+
+
+
+
+
+
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+
+//need to set up DB connection
+//require('./config/db.connection')
+
+
+const gamesController = require('./controllers/game_controller')
+
+
+//app configuration
+const app = express()
+const PORT = 4000
+app.set('view engine', 'ejs')
+
+
+//Middleware for each request
+app.use('/games', gamesController)
+
+
+//404 error route
+app.get('*', (req, res) =>{
+    res.render('404')
+})
+
+//server
+app.listen(4000, () => console.log('starting server at port:', PORT))
