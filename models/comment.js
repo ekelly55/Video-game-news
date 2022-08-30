c7c4537d678fa4a61a69fe78169579c16f91ee93
hazelrahbe <swp.hazel94@gmail.com>
2022-08-30 10:18:40 -0400

    Comment Schema

--- models/comment.js
+++ models/comment.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const commentSchema = new mongoose.Schema ({
-    comment: String,
+    comment: {type: String, required: [true, "please write a comment"]},
     rating: {type: String, default: 0, min: 0, max: 5},
+    game: {type: mongoose.Types.ObjectId, ref: "Games"}
 },
 {timestamps: true})
 
-const Comment = mongoose.model('Comment', reviewSchema)
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment
