6f402ab06c60a823ff4a2294af4d13e342e1bd7b
hazelrahbe <swp.hazel94@gmail.com>
2022-08-30 10:19:28 -0400

    Comment Schema 1

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const commentSchema = new mongoose.Schema ({
     comment: {type: String, required: [true, "please write a comment"]},
-    rating: {type: String, default: 0, min: 0, max: 5},
+    rating: {type: Number, default: 0, min: 0, max: 5},
     game: {type: mongoose.Types.ObjectId, ref: "Games"}
 },
 {timestamps: true})
