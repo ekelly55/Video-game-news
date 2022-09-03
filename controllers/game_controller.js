0165ecdb7390da1bfb7e5db8f0c139becc94d5e6
ekelly55 <eamonn.michael.kellyr@email.com>
2022-09-02 21:20:53 -0400

    comments display username

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -64,9 +64,8 @@ router.get('/new', (req,res) =>{
 router.get('/:id', async (req,res, next) =>{
     try{
         const foundGame = await db.Games.findById(req.params.id)
-        const gameComments = await db.Comment.find({game: foundGame._id}).
-        populate("user").exec()
-        console.log(res.locals.currentUser)
+        const gameComments = await db.Comment.find({game: foundGame._id}).populate("user").exec()
+        //console.log(res.locals.currentUser)
         const context = {game: foundGame, id: foundGame._id, comments: gameComments}
         //console.log(gameComments.user)
         //console.log(req.session.currentUser)
