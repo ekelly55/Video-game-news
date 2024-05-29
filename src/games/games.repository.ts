adcb2ee9838238c4cd87efb93ba427889efca624
ekelly55 <eamonn.michael.kellyr@email.com>
2024-05-29 16:15:21 -0400

    games.repository complete

--- src/games/games.repository.ts
+++ src/games/games.repository.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express"
 import methodOverride from 'method-override'
 import Games from "./games.interface";
 import { IGames } from "./games.interface";
+import Comment from "../comments/comments.interface";
 
 const gamesRouter = express.Router()
 
@@ -16,7 +17,7 @@ gamesRouter.get('/privacy_policy', async (req: Request, res: Response) => {
 });
 
 //GET about us
-gamesRouter.get('/about_us', (req, res) => {
+gamesRouter.get('/about_us', (req: Request, res: Response) => {
     res.render('aboutus.ejs');
 });
 
@@ -31,7 +32,7 @@ gamesRouter.post('/', async (req: Request, res: Response, next: NextFunction) =>
         const gameData = req.body as IGames;
         const foundGame = await Games.exists({name: gameData.name});
         if(!foundGame){
-            return res.redirect('/:id');
+            return res.status(400).send("Game already exists");
         } else {
             const newGame = await Games.create(gameData)
             return res.redirect('/games');
@@ -44,10 +45,33 @@ gamesRouter.post('/', async (req: Request, res: Response, next: NextFunction) =>
 });
 
 //GET games index
-export const findAll = async (): Promise<IGames[]> => Object.values(Games);
+gamesRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const allGames = await Games.find()
+        const context = {games: allGames};
+        res.render('games_index.ejs', context);
+    } catch(err) {
+        console.log(err)
+        res.status(500).send('Internal server error')
+    }
+});
 
 //GET game show page
-export const findOne = async (id: string): Promise<IGames> => Games[id];
+gamesRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const foundGame = await Games.findById(req.params.id);
+        if(!foundGame){
+            res.status(404).send('Game not found')
+            return;
+        }
+        const gameComments = await Comment.find({game: foundGame._id}).populate('user').exec()
+        const context = {game: foundGame, id: foundGame._id, comments: gameComments};
+        res.render('games_show.ejs,', context);
+    } catch(err){
+        console.log(err)
+        res.status(500).send('Internal server error')
+    }
+});
 
 //DELETE game
 gamesRouter.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
@@ -69,14 +93,9 @@ gamesRouter.get('/:id/edit', async (req: Request, res: Response, next: NextFunct
 
 gamesRouter.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const updatedGame = req.body as IGames
-        const foundGame = await Games.exists({name: updatedGame.name});
-        if(!foundGame){
-            return res.redirect('/:id');
-        } else {
-            const newGame = await Games.updateOne({id: updatedGame._id})
-            return res.redirect('/games');
-        }
+        const updatedGame = await Games.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect('/games')
+       
     }
     catch(err) {
         console.error(err);
